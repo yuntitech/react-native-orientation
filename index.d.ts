10c3601c7df58789f727450f94e65fb1c6664884
kangqiang <185920621@qq.com>
2020-02-26 21:15:45 +0800

    feat: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export function unlockAllOrientations(): void;
+
+export function getOrientation(
+  cb: (error: any, orientation: string) => void
+): void;
+
+export function addOrientationListener(cb: (orientation: string) => void): void;
+
+export function removeOrientationListener(
+  cb: (orientation: string) => void
+): void;
+
+export function lockToPortrait(): void;
+
+export function lockToLandscape(): void;
+
+export function lockToLandscapeRight(): void;
